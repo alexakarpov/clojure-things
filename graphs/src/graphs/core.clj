80768a76b468c68d9e3d36b82bb11b6cb0b973aa
Alexandre Karpov <alexakarpov@gmail.com>
2017-11-13 11:30:59 -0500

    back in saddle

--- graphs/src/graphs/core.clj
+++ graphs/src/graphs/core.clj
@@ -5,3 +5,10 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(comment
+  "(def node (Node. "foo" nil)) -- will create the instance of a Node JVM class
+   then, (.car node) is how you access its members (remember?)
+")
+
+(deftype Node [car cdr])
