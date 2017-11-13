baf0eb42dfdf4fdcc5e4265f9cb5f9d3bb1c1e9b
Alexandre Karpov <alexakarpov@gmail.com>
2017-11-13 11:44:43 -0500

    make fields of Node mutable

--- graphs/src/graphs/core.clj
+++ graphs/src/graphs/core.clj
@@ -11,4 +11,15 @@
    then, (.car node) is how you access its members (remember?)
 ")
 
-(deftype Node [car cdr])
+(definterface INode
+  (getCar [])
+  (getCdr [])
+  (setCar [x])
+  (setCar [x]))
+
+(deftype Node [^:volatile-mutable car ^:volatile-mutable cdr]
+  INode
+  (getCar [this] car)
+  (getCdr [this] cdr)
+  (setCar [this x] (set! car x) this)
+  (setCdr [this x] (set! cdr x) this))
