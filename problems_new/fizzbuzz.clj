9a9009dac2eb52bf5c11cf14bf67b9e7a3de77d5
Alexandre Karpov <alexakarpov@gmail.com>
2019-05-18 13:53:01 -0400

    Revert "fizbuzz"
    This reverts commit b0af21ee1ff81a45955a04f9f556aba258065e9d.

--- problems_new/fizzbuzz.clj
+++ problems_new/fizzbuzz.clj
@@ -1,12 +1,11 @@
 (ns problems.trees
   (:use [clojure.test])
-  (:require [clojure.string :refer [includes?]])
-  (:require [clojure.data.json :as j]))
+  (:use [clojure.string]))
 
 (defn fizzbuzz [n]
   (let [fizz? #(zero? (mod % 3))
         buzz? #(or
-                (includes? (str %) "5")
+                (includes? (str %) "5") 
                 (zero? (mod % 5)))]
     (cond
       (and (fizz? n) (buzz? n)) "FizzBuzz"
@@ -18,39 +17,18 @@
   (is (= (fizzbuzz 2)
          2))
   (is (= (fizzbuzz 3)
-         "Fizz"))
+         3))
   (is (= (map fizzbuzz (range 1 11))
          [1 2 "Fizz" 4 "Buzz" "Fizz" 7 8 "Fizz" "Buzz"]))
   (is (= (fizzbuzz 51)
          "FizzBuzz")))
 
-(defn rrec [n]
-  (loop
-      [x n]
-    (if (< x 0) "done"
-        (cond
-          (= x 123) (do
-                      (println  "magic number!f")
-                      :barf)
-          (odd? x) (do
-                     (println x "is odd")
-                     (recur (dec x)))
-          (even? x) (do
-                      (println x "is even")
-                      (recur (dec x)))
-          :else (do
-                  (println "lol?"))))))
+(fizzbuzz 3)
 
-(rrec 122)
-(println "hi")
-
-(comment
-  (let [nums (range 1 101)
-        fbz (map fizzbuzz nums)
-        pairz (map vector nums fbz)]
-    (doseq [[f s] pairz]
-      (println f s)))
-
-  (read-str (slurp "http://data.fixer.io/api/latest?access_key=a33bbf8f520b63070591137d4b05d435&symbols=USD")))
+(let [nums (range 1 101)
+      fbz (map fizzbuzz nums)
+      pairz (map vector nums fbz)]
+  (doseq [[f s] pairz]
+    (println f s)))
 
 (run-tests)
